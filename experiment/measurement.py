7d3f36273fe5ae22117481a9772d88e4d791bcef
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2017-12-16 11:07:09 +0100

    measurement class now based on BaseInstrument changed data saving location options

--- experiment/measurement.py
+++ experiment/measurement.py
@@ -10,14 +10,62 @@ import logging
 import json
 from qcodes.utils.helpers import DelegateAttributes, full_class
 from qcodes.utils.metadata import Metadatable
+from qcodes.instrument.base import InstrumentBase
 from qcodes.instrument.parameter import _BaseParameter
 
 import labpythonconfig as cfg
 
+DATAIDXPAD = 4
+
+def get_next_data_idx(folder, prefix):
+    if not os.path.exists(folder):
+        return "-#" + "0".zfill(DATAIDXPAD)
+
+    files = [os.path.splitext(f)[0] for f in os.listdir(folder)]
+    idxs = []
+    for f in files:
+        try:
+            idxs.append(int(f.split(prefix+"-#")[1].split("_")[0]))
+        except:
+            pass
+
+    if not idxs:
+        return "-#" + "0".zfill(DATAIDXPAD)
+
+    return "-#" + str(max(idxs) + 1).zfill(DATAIDXPAD)
+
+
+def get_data_loc(name, struct_time=None, makedir=False):
+    if struct_time is None:
+        struct_time = time.localtime()
+
+    datadir = time.strftime(cfg.data_location, struct_time)
+    datadir = datadir.replace("{n}", name)
+
+    datafile_prefix = time.strftime(cfg.datafile_prefix, struct_time)
+    datafile_prefix = datafile_prefix.replace("{n}", name)
+    if cfg.data_location_idx:
+        datafile_prefix += get_next_data_idx(datadir, datafile_prefix)
+
+    ret = {
+        'data_folder' : datadir,
+        'datafile_prefix' : datafile_prefix,
+    }
+
+    return ret
+
+# class BaseExperimentData(object):
+#     def __init__(self, name, **kwargs):
+#         self.name = name
+
+#     def add_data(self, **kwargs):
+#         raise NotImplementedError
+
 
 class Parameter(_BaseParameter):
+
     def __init__(self, name, initial_value=None, **kw):
-        super().__init__(name, None, **kw)
+        super().__init__(name, **kw)
         self._value = initial_value
 
     def get_raw(self):
@@ -27,29 +75,14 @@ class Parameter(_BaseParameter):
         self._value = value
 
 
-class BaseMeasurement(Metadatable, DelegateAttributes):
+class BaseMeasurement(InstrumentBase):
 
     def __init__(self, station):
-        super().__init__()
         self.station = station
-        self.msmt_name = self.__class__.__name__
+        name = self.__class__.__name__
+        super().__init__(name)
 
-    def snapshot_base(self, update=False, *arg, **kw):
-        snap = {
-            "__class__": full_class(self)
-            }
-
-        snap['parameters'] = {}
-        for name in self.__dir__():
-            if isinstance(getattr(self, name), _BaseParameter):
-                param = getattr(self, name)
-                try:
-                    snap['parameters'][name] = param.snapshot(update=update)
-                except:
-                    logging.debug("Snapshot: Could not update parameter: {}".format(name))
-                    snap['parameters'][name] = param.snapshot(update=False)
-
-        return snap
+        self._meta_attrs = ['name']
 
     def save_metadata(self):
         station_snap = self.station.snapshot()
@@ -66,11 +99,11 @@ class BaseMeasurement(Metadatable, DelegateAttributes):
         Find the folder on the file system where we will store all
         data and metadata coming from the measurement.
         """
-        folder, prefix = cfg.data_location_formatter
-        self.dataloc_folder = os.path.join(cfg.data_location,
-                                           time.strftime(folder)+"_{}".format(self.msmt_name))
-        self.datafile_prefix = time.strftime(prefix) + "_{}".format(self.msmt_name)
+        info = get_data_loc(self.name)
+        self.dataloc_folder = info['data_folder']
+        self.datafile_prefix = info['datafile_prefix']
         self.dataloc_prefix = os.path.join(self.dataloc_folder, self.datafile_prefix)
+        print(self.dataloc_prefix)
 
     def init_data(self):
         """
@@ -78,7 +111,7 @@ class BaseMeasurement(Metadatable, DelegateAttributes):
         """
         self._get_data_location()
         if os.path.exists(self.dataloc_folder):
-            logging.warning("Data folder {} already exists.".format(self.dataloc_folder))
+            logging.info("Data folder {} already exists.".format(self.dataloc_folder))
         else:
             os.makedirs(self.dataloc_folder)
 
--- templates/labpythonconfig.py
+++ templates/labpythonconfig.py
@@ -1,14 +1,20 @@
 import os
 
-# Base folder where we will store all data and metadata
-data_location = "c:\\data"
-
-# How to generate a location for each dataset.
-# need a tuple with two elements, each of which we will supply to
-# time.strftime().
-# first element: folder structure for the measurement.
-# second element: file name prefix for all generated files.
-data_location_formatter = "%Y-%m\\%Y-%m-%d\\%H%M%S", "%H%M%S"
+# all data location formats will be used with time.strftime()
+# additionally available variables:
+#  * {n} : name of the measurement
+#
+# The folder where to store a new data set
+data_location = "c:\\data\\%Y-%m\\%Y-%m-%d"
+# data file prefix in the data location
+datafile_prefix = "%Y-%m-%d_{n}"
+# subdirectory in the data location for metadata (can be "")
+metadata_subdir = "%Y-%m-%d_{n}"
+# prefix for metadata files
+metadatafile_prefix = "%Y-%m-%d_{n}"
+# whether to append a counting index to the data files
+# (handy when storing multiple data sets in the same folder)
+data_location_idx = True
 
 # the working directory of our code
 labpython_dir = ""
