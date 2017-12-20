51421ef589f4f2f35bf3344fe46b4cd66af61a4c
Wolfgang Pfaff <wolfgang.pfaff@microsoft.com>
2017-12-20 14:26:50 +0100

    added basic data handling and saving

--- experiment/measurement.py
+++ experiment/measurement.py
@@ -13,78 +13,19 @@ import os
 import time
 import logging
 import json
-import xarray as xr
+import h5py
+from collections import OrderedDict
+import numpy as np
 
-from qcodes.utils.helpers import DelegateAttributes, full_class
-from qcodes.utils.metadata import Metadatable
 from qcodes.instrument.base import InstrumentBase
 from qcodes.instrument.parameter import _BaseParameter
 
+from pysweep.data_storage import NpStorage
+
 import labpythonconfig as cfg
 
+
 DATAIDXPAD = 4
-
-def get_next_data_idx(folder, prefix):
-    if not os.path.exists(folder):
-        return "-#" + "0".zfill(DATAIDXPAD)
-
-    files = [os.path.splitext(f)[0] for f in os.listdir(folder)]
-    idxs = []
-    for f in files:
-        try:
-            idxs.append(int(f.split(prefix+"-#")[1].split("_")[0]))
-        except:
-            pass
-
-    if not idxs:
-        return "-#" + "0".zfill(DATAIDXPAD)
-
-    return "-#" + str(max(idxs) + 1).zfill(DATAIDXPAD)
-
-
-def get_data_loc(name, struct_time=None, makedirs=False):
-    if struct_time is None:
-        struct_time = time.localtime()
-    idx = None
-
-    datadir = time.strftime(cfg.data_location, struct_time)
-    datadir = datadir.replace("{n}", name)
-
-    datafile_prefix = time.strftime(cfg.datafile_prefix, struct_time)
-    datafile_prefix = datafile_prefix.replace("{n}", name)
-    if cfg.data_location_idx:
-        idx = get_next_data_idx(datadir, datafile_prefix)
-        datafile_prefix += idx
-
-    if cfg.metadata_subdir != "":
-        metadatadir = time.strftime(cfg.metadata_subdir, struct_time)
-        metadatadir = metadatadir.replace("{n}", name)
-        if idx is not None:
-            metadatadir += idx
-        metadatadir = os.path.join(datadir, metadatadir)
-    else:
-        metadatadir = datadir
-
-    metadatafile_prefix = time.strftime(cfg.datafile_prefix, struct_time)
-    metadatafile_prefix = metadatafile_prefix.replace("{n}", name)
-    if idx is not None:
-        metadatafile_prefix += idx
-
-    if makedirs:
-        if not os.path.exists(datadir):
-            os.makedirs(datadir)
-        if cfg.metadata_subdir != '':
-            if not os.path.exists(metadatadir):
-                os.makedirs(metadatadir)
-
-    ret = {
-        'data_folder' : datadir,
-        'datafile_prefix' : datafile_prefix,
-        'metadata_folder' : metadatadir,
-        'metadata_prefix' : metadatafile_prefix,
-    }
-
-    return ret
 
 
 class Parameter(_BaseParameter):
@@ -109,18 +50,63 @@ class BaseMeasurement(InstrumentBase):
 
         self._meta_attrs = ['name']
 
-    def _get_data_location(self, makedirs=True):
-        """
-        Find the folder on the file system where we will store all
-        data and metadata coming from the measurement.
-        """
-        info = get_data_loc(self.name, makedirs=makedirs)
-        self.data_folder = info['data_folder']
-        self.datafile_prefix = info['datafile_prefix']
-        self.data_prefix = os.path.join(self.data_folder, self.datafile_prefix)
-        self.metadata_folder = info['metadata_folder']
-        self.metadatafile_prefix = info['metadata_prefix']
-        self.metadata_prefix = os.path.join(self.metadata_folder, self.metadatafile_prefix)
+    @staticmethod
+    def _get_next_data_idx(folder, prefix):
+        if not os.path.exists(folder):
+            return "-#" + "0".zfill(DATAIDXPAD)
+
+        files = [os.path.splitext(f)[0] for f in os.listdir(folder)]
+        idxs = []
+        for f in files:
+            try:
+                idxs.append(int(f.split(prefix+"-#")[1].split("_")[0]))
+            except:
+                pass
+
+        if not idxs:
+            return "-#" + "0".zfill(DATAIDXPAD)
+
+        return "-#" + str(max(idxs) + 1).zfill(DATAIDXPAD)
+
+
+    def _get_data_location(self, makedirs=True, struct_time=None):
+        if struct_time is None:
+            struct_time = time.localtime()
+        idx = None
+
+        self.datadir = time.strftime(cfg.data_location, struct_time)
+        self.datadir = self.datadir.replace("{n}", self.name)
+
+        self.datafile_prefix = time.strftime(cfg.datafile_prefix, struct_time)
+        self.datafile_prefix = self.datafile_prefix.replace("{n}", self.name)
+        if cfg.data_location_idx:
+            idx = BaseMeasurement._get_next_data_idx(self.datadir, self.datafile_prefix)
+            self.datafile_prefix += idx
+
+        self.data_prefix = os.path.join(self.datadir, self.datafile_prefix)
+
+        if cfg.metadata_subdir != "":
+            self.metadatadir = time.strftime(cfg.metadata_subdir, struct_time)
+            self.metadatadir = self.metadatadir.replace("{n}", self.name)
+            if idx is not None:
+                self.metadatadir += idx
+            self.metadatadir = os.path.join(self.datadir, self.metadatadir)
+        else:
+            self.metadatadir = self.datadir
+
+        self.metadatafile_prefix = time.strftime(cfg.datafile_prefix, struct_time)
+        self.metadatafile_prefix = self.metadatafile_prefix.replace("{n}", self.name)
+        if idx is not None:
+            self.metadatafile_prefix += idx
+
+        self.metadata_prefix = os.path.join(self.metadatadir, self.metadatafile_prefix)
+
+        if makedirs:
+            if not os.path.exists(self.datadir):
+                os.makedirs(self.datadir)
+            if cfg.metadata_subdir != '':
+                if not os.path.exists(self.metadatadir):
+                    os.makedirs(self.metadatadir)
 
 
     def init_data(self):
@@ -129,9 +115,7 @@ class BaseMeasurement(InstrumentBase):
         """
         self._get_data_location(makedirs=True)
         self.datafilepath = self.data_prefix + ".hdf5"
-
-        # self.dataset = xr.Dataset()
-        # self.save_data()
+        self.data = Data(self.datafilepath)
 
     def save_metadata(self):
         station_snap = self.station.snapshot()
@@ -142,16 +126,12 @@ class BaseMeasurement(InstrumentBase):
         with open(self.metadata_prefix + "_measurement.json", 'w') as f:
             json.dump(msmt_snap, f, indent=4)
 
-    def save_data(self):
-        # mode = 'a' if os.path.exists(self.datafilepath) else 'w'
-        # self.dataset.to_netcdf(self.datafilepath, mode, format='NETCDF4')
-        pass
 
     def pre_measurement_tasks(self):
         self.init_data()
 
+
     def post_measurement_tasks(self):
-        self.save_data()
         self.save_metadata()
 
 
@@ -180,3 +160,118 @@ class BaseMeasurement(InstrumentBase):
 
     def cleanup(self):
         pass
+
+
+class Data(NpStorage):
+    def __init__(self, filepath):
+        super().__init__()
+        self.filepath = filepath
+        self.export_spyview = True
+
+
+    @staticmethod
+    def flatten_page(page, invert_axis_order=True):
+        names = [n for n in page.dtype.fields]
+        if invert_axis_order:
+            names = names[:-1][::-1] + [names[-1]]
+
+        shapes = [page.dtype[n].shape for n in names]
+        dtypes = [page[n].dtype for n in names]
+        sizes = [np.prod(s) for s in shapes]
+
+        if ((1,) in shapes and len(set(shapes)) > 2) or ((1,) not in shapes and len(set(shapes)) > 1):
+            raise NotImplementedError("Flattening of non-trivial shapes is currently not implemented.")
+
+        blocksize = max(sizes)
+        pagelen = page.size
+        datasize = blocksize * pagelen
+        data = np.zeros((datasize,), dtype=[(n,d) for n,d in zip(names, dtypes)])
+
+        for name, size in zip(names, sizes):
+            if size < blocksize:
+                data[name] = np.vstack(blocksize*[page[name].reshape(-1)]).reshape(-1, order='F')
+            else:
+                data[name] = page[name].reshape(-1)
+
+        return data
+
+
+    @staticmethod
+    def meta_info(data, exclude_dependent=True):
+        ret = OrderedDict({})
+        names = [n for n in data.dtype.fields]
+        if exclude_dependent:
+            names = names[:-1]
+
+        for n in names:
+            vals, nvals = np.unique(data[n], return_counts=True)
+            ret[n] = {
+                'values' : vals,
+                'occurences' : nvals,
+            }
+        return ret
+
+
+    def write_spyview_meta(self, fn, pagename):
+        data = Data.flatten_page(self[pagename], invert_axis_order=True)
+        info = Data.meta_info(data)
+        names = [n for n in data.dtype.fields]
+        with open(fn, 'w') as f:
+            for idx, n in enumerate(names[:-1]):
+                m = info[n]
+                f.write("{}\n{}\n{}\n{}\n".format(m['values'].size,
+                                                  m['values'][0],
+                                                  m['values'][-1],
+                                                  n))
+            f.write("{}\n{}\n".format(idx+2, names[-1]))
+
+
+    def write_spyview_data(self, fn, page):
+        isnewfile = not os.path.exists(fn)
+
+        data = Data.flatten_page(page, invert_axis_order=True)
+        names = [n for n in data.dtype.fields]
+        pagename = names[-1]
+
+        alldata = Data.flatten_page(self[pagename], invert_axis_order=True)
+        col0_start = alldata[names[0]][0]
+
+        metafn = os.path.splitext(fn)[0] + ".meta.txt"
+        self.write_spyview_meta(metafn, pagename)
+
+        with open(fn, 'a') as f:
+            for i, row in enumerate(data):
+                if row[names[0]] == col0_start:
+                    f.write("\n")
+
+                line = "\t".join(["{}".format(row[k]) for k in names])
+                line += "\n"
+                f.write(line)
+
+
+    def add(self, record, write=True):
+        super().add(record)
+        if write:
+            self.write(record)
+
+
+    def write(self, record=None):
+        with h5py.File(self.filepath, 'a') as f:
+            if record is None:
+                pages = self._pages
+            else:
+                pages = self.record_to_pages(record)
+
+            for name, arr in pages.items():
+                if name in f:
+                    d = f[name]
+                else:
+                    d = f.create_dataset(name, (0,), maxshape=(None,), dtype=arr.dtype, chunks=True)
+
+                newlen = d.size + arr.size
+                d.resize((newlen,))
+                d[-arr.size:] = arr
+
+                if self.export_spyview:
+                    spyview_fn = os.path.splitext(self.filepath)[0] + "_{}.dat".format(name)
+                    self.write_spyview_data(spyview_fn, arr)
