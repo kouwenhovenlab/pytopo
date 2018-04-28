b32ff26574570c0d56f4cee8dbe15564ba3a7a92
Wolfgang Pfaff <wolfgang.pfaff@microsoft.com>
2018-04-28 18:13:00 +0200

    started making a proper package out of it.

---
+++ pytopo/qctools/dataset2.py
@@ -0,0 +1,49 @@
+""" A module to make working with the qcodes v2 dataset a bit more
+convenient.
+
+Maintainer: Wolfgang Pfaff <wolfgangpfff@gmail.com>
+"""
+
+import time
+import qcodes as qc
+from qcodes.dataset.data_set import DataSet
+from qcodes.dataset.experiment_container import load_experiment_by_name, new_experiment
+from qcodes.dataset.sqlite_base import transaction, one, get_dependencies, get_dependents, get_layout
+
+def select_experiment(exp_name, sample_name):
+    """
+    Convenience function that will check if the experiment/sample
+    combination already exists in the current database. If so,
+    it'll return the existing one. Otherwise it will create a new
+    one and return that.
+
+    Potential issue: if multiple experiments with the same
+    experiment/sample combination exist, our detection method will
+    fail, and another copy of this combination will be created.
+    """
+    try:
+        exp = load_experiment_by_name(exp_name)
+    except ValueError:
+        exp = new_experiment(exp_name, sample_name)
+    return exp
+
+
+def get_run_timestamp(run_id):
+    DB = qc.config["core"]["db_location"]
+
+    d = DataSet(DB)
+    sql = """
+    SELECT run_timestamp
+    FROM
+      runs
+    WHERE
+      run_id= ?
+    """
+    c = transaction(d.conn, sql, run_id)
+    run_timestamp = one(c, 'run_timestamp')
+    return run_timestamp
+
+
+def timestamp_to_fmt(ts, fmt="%Y-%m-%d %H:%M:%S"):
+    return time.strftime(fmt, time.gmtime(ts))
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='pytopo',
+    version='0.0.1',
+    description='shared tools for experiments in the QuTech Topo group',
+    author='Wolfgang Pfaff',
+    author_email='wolfgangpfff@gmail.com',
+    url='https://github.com/kouwenhovenlab',
+)
