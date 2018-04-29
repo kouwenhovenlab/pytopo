57384a1ab4023fd567076a7b4ffcf889958ed687
Wolfgang Pfaff <wopfa@microsoft.com>
2018-04-29 09:42:47 +0200

    moved plotting tools from labtools in here

---
+++ pytopo/mplplots/init_nb_plotting.py
@@ -0,0 +1,12 @@
+from matplotlib import rcParams
+
+rcParams['figure.dpi'] = 150
+rcParams['figure.figsize'] = (4.5, 3)
+rcParams['savefig.dpi'] = 150
+rcParams['savefig.transparent'] = False
+rcParams['font.family'] = 'Arial'
+rcParams['font.size'] = 8
+rcParams['lines.markersize'] = 4
+rcParams['axes.facecolor'] = 'w'
+rcParams['figure.facecolor'] = 'w'
+rcParams['nbagg.transparent'] = False
