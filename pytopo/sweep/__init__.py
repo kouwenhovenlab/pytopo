8ad56fe999e62c017c6e2b0dcbc94cb52d5be081
sohail chatoor <schatoor@gmail.com>
2018-08-01 15:15:34 +0200

    added nest and chain to __init__

--- pytopo/sweep/__init__.py
+++ pytopo/sweep/__init__.py
@@ -1,3 +1,3 @@
-from .convenience import sweep, measure, time_trace, call
+from .convenience import sweep, measure, time_trace, call, nest, chain
 from .getter_setter import getter, setter
 from .do_experiment import do_experiment
