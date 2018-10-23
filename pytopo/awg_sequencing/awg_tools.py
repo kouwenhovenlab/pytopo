7f796e9675cfa8884287941702dad71c9912853b
Local Administrator <LocalAdmin@TUD278005.tudelft.net>
2018-10-23 14:17:24 +0200

    model-independent trigger

---
+++ pytopo/awg_sequencing/awg_tools.py
@@ -0,0 +1,17 @@
+from qcodes.instrument_drivers.tektronix.AWG5014 import Tektronix_AWG5014
+from qcodes.instrument_drivers.tektronix.AWG5208 import AWG5208
+
+def trigger_awg_when_ready(awg):
+    """
+    AWG-model independent trigger. waits until the AWG is ready to receive a trigger.
+    """
+    if isinstance(awg, AWG5208):
+        while awg.run_state() != 'Waiting for trigger':
+            time.sleep(0.01)
+        awg.force_triggerA()
+    else:
+        while awg.get_state() != 'Waiting for trigger':
+            time.sleep(0.01)
+        awg.force_trigger()
+    
+    return True
